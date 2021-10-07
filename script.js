8046d44ccc2102e67f1f0d75583a70ee5cfbd04b
jeetu97 <86956247+jeetu97@users.noreply.github.com>
2021-10-07 14:13:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+    function time(){
+     var date=new Date();
+     var hours= date.getHours();
+     var min=date.getMinutes();
+     var sec= date.getSeconds();
+     
+     if(hr=="12"){
+         var hr=12; // 12 hours time
+      }
+      else if(hr=="24"){
+          var hr="0";
+      }
+    else{
+        var hr=hours%12;
+    }
+    if (hr<10)
+    {
+        hr="0"+hr;
+    }
+    if (min<10)
+    {
+        min="0"+min;
+    }
+    if(sec<10)
+    {
+        sec="0"+sec;
+    }  
+   
+     var ampm=hours<12?"AM":"PM";
+     var timed=hr+":"+min+":"+sec+":"+ampm;
+     document.getElementById('clock').innerText=timed;
+    }  
+    setInterval(time,1000);  
+    
